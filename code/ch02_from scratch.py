74e8f632039aef58f98bb720645b4ec7bd6c4cb0
clan2000 <ethan.skt@gmail.com>
2016-08-18 18:35:11 +0900

    내용 추가

--- code/ch02_from scratch.py
+++ code/ch02_from scratch.py
@@ -1,12 +1,34 @@
 def double(x):
     return x * 2
 
-def apply_to_one(f):            # 인자로 들어온 함수 f에 1을 넣는 함수
+def apply_to_one(f):         # 인자로 들어온 함수 f에 1을 넣는 함수
+                                # 의미 없는 라인
     return f(1)
 
 my_double = double              # 함수에 함수를 대입할수 있다
 x = apply_to_one(my_double)     # 함수를 인자로 하여 다른 함수에 대입할수 있다.
 
-print (x)
+print(x)
 
+y = apply_to_one(lambda x: x+4)  # 5  짧은 익명 람다 함수
 
+another_double = lambda x: 2 * x
+
+print(y)
+
+another_double = lambda x: 2 * x        # don't do this
+def another_double(x): return 2 * x     # do this instead
+
+def my_print(message="my default message"):  # message 인자의 Default 값으로 my default message를 사용
+    print (message)
+
+my_print("hello") # prints 'hello'
+my_print() # prints 'my default message'
+
+my_print("dododo")
+
+def subtract(a=0, b=0):
+    return a - b
+print (subtract(10, 5)) # returns 5
+print (subtract(0, 5)) # returns -5
+print (subtract(b=5)) # same as previous
